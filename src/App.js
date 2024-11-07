7947c72f810aef62b6cb97ffc6b61a5bab250fc4
Absher K <absheroamac@gmail.com>
2024-11-08 00:59:04 +0530

    Completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [isLogined, setIsLogined] = useState(false);
+  const [isInvalid, setIsInvalid] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (username === "user" && password === "password") {
+      setIsLogined(true);
+    } else {
+      setIsInvalid(true);
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Login Page</h1>
+      {isInvalid && <p>Invalid username or password</p>}
+      {isLogined && <p>Welcome, user!</p>}
+      {!isLogined && (
+        <form onSubmit={handleSubmit}>
+          <label>
+            Username:
+            <input
+              placeholder="username"
+              onChange={(event) => setUsername(event.target.value)}
+            ></input>
+          </label>
+          <br />
+          <label>
+            Password:
+            <input
+              type="password"
+              placeholder="password"
+              onChange={(event) => setPassword(event.target.value)}
+            ></input>
+          </label>
+          <br />
+          <button type="submit">Submit</button>
+        </form>
+      )}
     </div>
   );
 }
